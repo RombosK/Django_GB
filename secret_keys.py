f4f669d62f6f84f7425534670240493ff11c1a43
Roman <89097310+RombosK@users.noreply.github.com>
2022-06-16 00:55:59 +0300

    Delete secret_keys.py

--- secret_keys.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = 'django-insecure-e@fxn*bi#)rga)(kh794ym@%@nflv=&3pc6%63!=sh@_8ny2=%'
-
-SOCIAL_AUTH_GITHUB_KEY = '3114b81827fa7e7b5698'
-
-SOCIAL_AUTH_GITHUB_SECRET = 'b26119d78736eafb03b0b0f07e3745253cf1528e'
