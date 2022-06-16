fc806425df42c3a7afabce9e944fdb16d7f6c0c5
Roman <rombos1978@gmail.com>
2022-06-16 20:40:47 +0300

    (settings_changed)

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
+    'django-extensions',
 
     'social_django',
     'crispy_forms',
